d311cde325ce05c3e92e1923dac1b9a7e087fde0
avih8r <sparco.209@gmail.com>
2018-05-02 09:16:52 +1000

    success message added

--- backup.py
+++ backup.py
@@ -7,6 +7,7 @@ config.read('backup.ini')
 backupSources = config['DEFAULT']['backupSources'].split(",")
 backupdDestinations = config['DEFAULT']['backupDestinations'].split(",")
 roboCopyOptions = config['DEFAULT']['roboCopyOptions'][1:-1]
+successMessage=''
 for source in backupSources:
     source = source.strip()
     for destination in backupdDestinations:
@@ -14,5 +15,8 @@ for source in backupSources:
         folderName=re.search('.*(\\\.+)',source).groups()[0]
         try:
             os.system('robocopy {bs} {rco} {bl}{fn}'.format(bs=source,rco=roboCopyOptions,bl=destination,fn=folderName))
+            successMessage+='>"{s}" has been successfully backed up to {b}\n'.format(s=folderName[1:-1],b=destination)
         except:
+            successMessage+='>"{s}" back up to {b} has failed\n'.format(s=folderName[1:-1],b=destination)
             pass
+print(successMessage)
