68bcf3d852f5b43aaeb83c74c6460a1c6061d916
avih8r <sparco.209@gmail.com>
2018-05-22 14:21:06 +1000

    streamlined use of slice ops, added encyption with 7zip

--- backup.py
+++ backup.py
@@ -1,7 +1,14 @@
 import sys, os
 import configparser
 import re
-sys.path.append(os.getcwd())
+import hashlib
+import getpass
+def generatePassHash():
+    temp = getpass.getpass().encode('utf-8')
+    return hashlib.sha224(temp).hexdigest()
+password=generatePassHash()
+cwd = os.getcwd()
+sys.path.append(cwd)
 config = configparser.ConfigParser()
 config.read('backup.ini')
 backupSources = config['DEFAULT']['backupSources'].split(",")
@@ -12,11 +19,35 @@ for source in backupSources:
     source = source.strip()
     for destination in backupdDestinations:
         destination = destination.strip()
-        folderName=re.search('.*(\\\.+)',source).groups()[0]
-        try:
-            os.system('robocopy {bs} {rco} {bl}{fn}'.format(bs=source,rco=roboCopyOptions,bl=destination,fn=folderName))
-            successMessage+='>"{s}" has been successfully backed up to {b}\n'.format(s=folderName[1:-1],b=destination)
-        except:
-            successMessage+='>"{s}" back up to {b} has failed\n'.format(s=folderName[1:-1],b=destination)
-            pass
+        folderName=re.search('.*(\\\.+)',source).groups()[0][1:-1]
+        #print(folderName)
+        #print('COMMAND IS'+'7za a -mhe=on -p{p} {fn}.7z {d}'.format(fn=folderName[1:-1],p=password,d=source))
+#'''
+        if re.search('.*encrypted.*',destination):
+            try:
+                #os.system('cd {d}'.format(dest=destination))
+                print('FOLDER NAME IS '+folderName)
+                print('\nCREATING ARCHIVE')
+                os.system('7za a -mhe=on -p{p} {fn}.7z {d}'.format(fn=folderName,p=password,d=source))
+                print('\nCREATING DIRECTORY')
+                os.system('mkdir {d}\{fn}"'.format(fn=folderName,d=destination[:-1]))
+                print('\nCOPYING ARCHIVE TO NEW DIRECTORY')
+                os.system('copy {fn}.7z {d}\{fn}"'.format(fn=folderName,d=destination[:-1]))
+                #print('COPY COMMAND IS '+'copy {fn}.7z {d}\{fn}"'.format(fn=folderName,d=destination[:-1]))
+                print('\nREMOVING SOURCE ARCHIVE')
+                os.system('del {fn}.7z'.format(fn=folderName))
+                #print('DEL COMMAND IS '+'del {fn}.7z'.format(fn=folderName))
+                #print('ROBOCOPY COMMAND IS '+'robocopy {bs} {rco} {bl}\{fn}"'.format(bs=source,rco=roboCopyOptions,bl=destination[:-1],fn=folderName))
+                successMessage+='>"{s}" has been successfully backed up securely to {b}\n'.format(s=folderName,b=destination)
+            except:
+                successMessage+='>"{s}" back up to {b} has failed\n'.format(s=folderName,b=destination)
+                pass
+        else:
+            try:
+                os.system('robocopy {bs} {rco} {bl}\{fn}"'.format(bs=source,rco=roboCopyOptions,bl=destination[:-1],fn=folderName))
+                successMessage+='>"{s}" has been successfully backed up to {b}\n'.format(s=folderName,b=destination)
+            except:
+                successMessage+='>"{s}" back up to {b} has failed\n'.format(s=folderName,b=destination)
+                pass
+#'''
 print(successMessage)
