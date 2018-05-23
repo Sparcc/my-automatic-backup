61cc93ed9915b70b55b39f96c9ff219a33531812
avih8r <sparco.209@gmail.com>
2018-05-23 14:10:26 +1000

    added automatic deployment

---
+++ deploy.py
@@ -0,0 +1,24 @@
+import sys, os
+import configparser
+def str2bool(v):
+    returnValue = False
+    if v.lower() in ("yes", "true", "t"):
+        returnValue = True
+    elif v.lower() in ("no", "false", "f"):
+        returnValue = False
+    return returnValue
+config = configparser.ConfigParser()
+config.read('deploy.ini')
+items = config['DEFAULT']['items'].split(",")
+locations = config['DEFAULT']['locations'].split(",")
+ignoreConfiguration = str2bool(config['DEFAULT']['ignoreConfiguration'])
+for item in items:
+    item = item.strip()[1:-1]
+    print('LOOKING AT ITEM '+item)
+    if not ignoreConfiguration and item[len(item)-3:] != "ini":
+        for location in locations:
+            location = location.strip()
+            os.system('mkdir {l}'.format(l=location))
+            os.system('copy {i} {l}'.format(i=item,l=location))
+    else:
+        print('Skipping configuration file: '+item)
